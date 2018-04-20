37ccdd6101127fb4ac769f4c95e52e5eba925d5f
Jaime Cervantes Velasco <jaime.cervantes@globant.com>
2018-04-19 19:51:54 -0500

    refactor(all): Enhancing HOC component for creating forms and reuse code

--- src/HOC/HOCSpForm.js
+++ src/HOC/HOCSpForm.js
@@ -4,7 +4,7 @@ function HOCSpForm(FormComponent) {
   return class extends React.Component  {
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = this.props.state || {};
     }
 
     handleOnChange = (e) => {
@@ -14,12 +14,27 @@ function HOCSpForm(FormComponent) {
       if (target.type === 'select-multiple') {
         value = Array.prototype.slice.call(target.options, 0).filter(opt => opt.selected).map(opt => opt.value)
       }
-  
       this.setState({ [name]: value });
-    }
+    };
+
+    handleOnSubmit = (e) => {
+      e.preventDefault();
+      if(this.props.onSubmit) {
+        this.props.onSubmit(this.state);
+      }
+    };
 
     render () {
-      return (<FormComponent onChange={this.handleOnChange} {...this.props}></FormComponent>);
+      // only re-pass props that the FormComponent needs. The porps state and onSubmit are used here in this HOC
+      // passProps are props not used from this HOC
+      const { state, onSubmit, ...passProps } = this.props;
+      // Pass props that are injected from this HOC
+      const injectedProps = { onSubmit: this.handleOnSubmit, onChange: this.handleOnChange, data: this.state };
+
+      return (<FormComponent
+        injectedProps={injectedProps}
+        {...passProps}>
+        </FormComponent>);
     }
   };
 }
--- src/SpSave/SpSave.js
+++ src/SpSave/SpSave.js
@@ -29,25 +29,32 @@ class SpSaveForm extends React.Component {
   }
 
   render () {
-    return (<form className="sp-save" action={this.props.action}>
+    const { onChange, data, onSubmit } = this.props.injectedProps;
+    return (<form className="sp-save" action={this.props.action} onSubmit={onSubmit}>
         <label>
           Nombre o título
+          {/*
+              If some data was gotten from a parent component or from the HOC component, the state is controlled inside the HOC component
+            */}
           <input 
             name="name"
-            onChange={this.props.onChange}
+            value={data.name || ''}
+            onChange={onChange}
             />
         </label>
         <label> Contenido
           <textarea
             name="content"
-            onChange={this.props.onChange}>
+            value={data.content}
+            onChange={onChange}>
           </textarea>
         </label>
         <label>
           Categorías:
           <select 
             name="categories"
-            onChange={this.props.onChange}
+            defaultValue={data.categories}
+            onChange={onChange}
             multiple={true}>
               {this.categories.map(tag => <option key={tag.id} value={tag.id}>{tag.name}</option>)}
           </select>
@@ -57,7 +64,8 @@ class SpSaveForm extends React.Component {
           Tiendas:
           <select 
             name="stores"
-            onChange={this.props.onChange}
+            defaultValue={data.stores}
+            onChange={onChange}
             multiple>
               {this.stores.map(tag => <option key={tag.id} value={tag.id}>{tag.name}</option>)}
           </select>
